2e8d6ea07e87da1e724eb72b4c7412fa62909ece
Mitchell Brenner <mitchbrenner8@gmail.com>
2025-07-01 15:15:41 -0600

    Implemented Tab navigation

---
+++ src/app/(app)/(tabs)/_layout.tsx
@@ -0,0 +1,70 @@
+import { Tabs } from "expo-router";
+import { AntDesign } from "@expo/vector-icons";
+
+const Layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="exercises"
+        options={{
+          title: "Exercises",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="book" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="workout"
+        options={{
+          title: "Workouts",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="pluscircle" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="active-workout"
+        options={{
+          title: "Active Workout",
+          headerShown: false,
+          href: null,
+          tabBarIcon: () => null,
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircleo" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default Layout;
